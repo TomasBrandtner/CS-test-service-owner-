b4adc7a831cab2ea869e1f6048eb45fa371af032
TomasBrandtner <137199880+TomasBrandtner@users.noreply.github.com>
2023-12-12 22:59:23 +0100

    Update ČS úkol 1.py

--- ČS úkol 1.py
+++ ČS úkol 1.py
@@ -1,20 +1,24 @@
-# Načtení obsahu jednotlivých souborů
-with open('1.txt', 'r') as soubor1:
-    obsah1 = soubor1.read()
+# Otevření a načtení obsahu jednotlivých souborů
+with open('1.txt', 'r') as soubor1:  # Otevření souboru '1.txt' v režimu čtení
+    obsah1 = soubor1.read()  # Načtení obsahu souboru '1.txt' a uložení do proměnné 'obsah1'
 
-with open('2.txt', 'r') as soubor2:
-    obsah2 = soubor2.read()
+with open('2.txt', 'r') as soubor2:  # Otevření souboru '2.txt' v režimu čtení
+    obsah2 = soubor2.read()  # Načtení obsahu souboru '2.txt' a uložení do proměnné 'obsah2'
 
-with open('3.txt', 'r') as soubor3:
-    obsah3 = soubor3.read()
+with open('3.txt', 'r') as soubor3:  # Otevření souboru '3.txt' v režimu čtení
+    obsah3 = soubor3.read()  # Načtení obsahu souboru '3.txt' a uložení do proměnné 'obsah3'
 
-# Seřazení obsahu souborů podle počtu řádků
-seřazený_obsah = [obsah1, obsah3, obsah2]
+# Vytvoření seznamu obsahů souborů spolu s jejich názvy a počtem řádků
+data_souboru = [  # Vytvoření seznamu obsahujícího názvy souborů a jejich obsahy a počty řádků
+    ('1.txt', obsah1, obsah1.count('\n') + 1),  # Tuple obsahující název souboru, obsah a počet řádků pro '1.txt'
+    ('2.txt', obsah2, obsah2.count('\n') + 1),  # Tuple obsahující název souboru, obsah a počet řádků pro '2.txt'
+    ('3.txt', obsah3, obsah3.count('\n') + 1)  # Tuple obsahující název souboru, obsah a počet řádků pro '3.txt'
+]
 
-# Vytvoření výsledného souboru
-with open('merged_files.txt', 'w') as spojený_soubor:
-    for i, obsah in enumerate(seřazený_obsah, start=1):
-        řádky = obsah.count("\n") + 1
-        spojený_soubor.write(f'{i}.txt\n{řádky}\n')
-        spojený_soubor.write(obsah)
-        spojený_soubor.write('\n\n')
+# Seřazení seznamu podle počtu řádků
+data_souboru.sort(key=lambda x: x[2])  # Seřazení seznamu podle třetího prvku (počet řádků) v každém tuple
+
+# Zápis do výsledného souboru
+with open('merget_files.txt', 'w') as spojený_soubor:  # Otevření souboru 'merget_files.txt' v režimu zápisu
+    for název_souboru, obsah, počet_řádků in data_souboru:  # Procházení seznamu souborů podle jejich počtu řádků
+        spojený_soubor.write(f"{název_souboru}\n{počet_řádků}\n{obsah}\n\n")  # Zápis názvu souboru, počtu řádků a obsahu souboru do výsledného souboru
